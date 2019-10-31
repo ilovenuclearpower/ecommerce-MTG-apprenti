cb8f29cdd8b9f2977cd717ad3266b309dbdf1bcd
Matza <cmatza@amazon.com>
2019-10-31 15:55:31 -0700

    Working on async requests for the cart pages

--- appdirectory/mtgstore/cart/models.py
+++ appdirectory/mtgstore/cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Card(models.Model):
+    imagelink = models.CharField(max_length=255)
+    tcgplayerid = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    gatherertext = models.TextField(max_length=255)
+    storeprice = models.FloatField()
+
+
--- appdirectory/mtgstore/cart/views.py
+++ appdirectory/mtgstore/cart/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
+from django.contrib import messages
+import asyncio
+import requests
+import json
+
+async def get_random_cards():
+    inventory = []
+
+    while len(inventory) < 10:
+        url = 'https://api.scryfall.com/cards/random'
+        value = await requests.get(url)
+        responseJson = value.json()
+        card = {
+            'name':responseJson['name'],
+            'price': responseJson['prices']['usd'],
+            'image': responseJson['image_uris']['large'],
+            'oracleText': responseJson['oracle_text'],
+        }
+        inventory.append(card)
+    return inventory
+
+def show_cart(request):
+    loop = asyncio.new_event_loop()
+    cart = loop.run_until_complete(get_random_cards())
+    return render(request, 'cart/cart.html', cart)
 
 # Create your views here.
